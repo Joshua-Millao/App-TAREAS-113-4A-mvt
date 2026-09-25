edd0ad25460f5a5003df2cbe6927a5a4422d201d
Talleres Puente Alto <tallerespa@inacapmail.cl>
2026-09-25 09:57:54 -0300

    Nueva actualizacion

---
+++ readme.txt
@@ -0,0 +1,12 @@
+winget install Python.Python.3.12
+py -3.12 --version
+git --version
+cd Desktop
+cd 24-09-2026
+git clone https://github.com/ericRailef/App-Tareas.git
+cd App-Tareas
+py -3.12 -m venv venv
+venv\Scripts\activate
+pip install -r requirements.txt
+python manage.py check
+python manage.py runserver
--- tareasapp/views.py
+++ tareasapp/views.py
@@ -17,7 +17,7 @@ def crear_tarea(request):
     if request.method == 'POST':
         titulo = request.POST['titulo']
         descripcion = request.POST['descripcion']
-        completada = 'completa' in request.POST
+        completada = 'completada' in request.POST
 
         Tarea.objects.create(
             titulo = titulo,
@@ -26,3 +26,37 @@ def crear_tarea(request):
         )
         return redirect('inicio')
     return render(request, 'tareasapp/crear.html')
+
+def detalle_tarea(request, id):
+    tarea = Tarea.objects.get(id=id)
+
+    return render(request, 'tareasapp/detalle.html', {
+        'tarea': tarea
+    })
+
+def editar_tarea(request, id):
+    tarea = Tarea.objects.get(id=id)
+    
+    if request.method == 'POST':
+        tarea.titulo = request.POST['titulo']
+        tarea.descripcion = request.POST['descripcion']
+        tarea.completada = 'completada' in request.POST
+
+        tarea.save()
+        return redirect('inicio')
+
+    return render(request, 'tareasapp/editar.html', {
+            'tarea': tarea
+        })
+
+def eliminar_tarea(request, id):
+    tarea = Tarea.objects.get(id=id)
+
+    if request.method == 'POST':
+        tarea.delete()
+
+        return redirect('inicio')
+    
+    return render(request, 'tareasapp/eliminar.html',{
+        'tarea': tarea
+    })
